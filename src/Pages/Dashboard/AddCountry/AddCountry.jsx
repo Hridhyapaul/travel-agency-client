fb198f7ace515cceea7acd38ec79bb2489adcde2
Hridhyapaul <hpdesign2021@gmail.com>
2023-08-16 22:51:48 +0600

    Created Add Country component to add country details

---
+++ src/Pages/Dashboard/AddCountry/AddCountry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCountry = () => {
+    return (
+        <div className='flex justify-center items-center py-20'>
+            <h1 className="text-3xl font-bold mb-10">Add Country Details</h1>
+        </div>
+    );
+};
+
+export default AddCountry;
