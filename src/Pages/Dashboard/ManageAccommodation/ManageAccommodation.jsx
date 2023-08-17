63114a46b7a809cfef37f93498d077d4881eeb80
Hridhyapaul <hpdesign2021@gmail.com>
2023-08-17 14:25:54 +0600

    Fetch accommodations to manage accommodation and used table to show accommodation

--- src/Pages/Dashboard/ManageAccommodation/ManageAccommodation.jsx
+++ src/Pages/Dashboard/ManageAccommodation/ManageAccommodation.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import usePopularDestination from '../../../Hooks/usePopularDestination';
+import ManageAccommodationCard from './ManageAccommodationCard';
 
 const ManageAccommodation = () => {
+    const [destinations, loading, refetch] = usePopularDestination();
+    console.log(destinations)
     return (
         <div className='py-20'>
-            <h1 className="text-3xl font-bold mb-10 text-center">Manage Accommodation</h1>
+            <h2 className='text-4xl font-body font-semibold text-center pb-4'>Manage Accommodation</h2>
+            <hr className='my-4' />
+            <div className='mt-8'>
+                <div className="overflow-x-auto">
+                    <table className="table font-body">
+                        {/* head */}
+                        <thead>
+                            <tr className='text-center text-[14px]'>
+                                <th></th>
+                                <th>Accommodation Name</th>
+                                <th>Country Name</th>
+                                <th>Location</th>
+                                <th>Price/person</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                destinations.map((destination, index) => <ManageAccommodationCard key={destination._id} destination={destination} index={index}></ManageAccommodationCard>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/ManageAccommodation/ManageAccommodationCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageAccommodationCard = ({destination, index}) => {
+    console.log(destination)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManageAccommodationCard;
