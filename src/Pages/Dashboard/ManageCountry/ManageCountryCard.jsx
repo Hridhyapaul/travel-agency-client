8334d0171c159e30cdb4c5c25f0017b3daeef8aa
Hridhyapaul <hpdesign2021@gmail.com>
2023-08-18 00:24:33 +0600

    Implemented table to manage country page and shown country data

--- src/Pages/Dashboard/ManageCountry/ManageCountryCard.jsx
+++ src/Pages/Dashboard/ManageCountry/ManageCountryCard.jsx
@@ -4,10 +4,19 @@ const ManageCountryCard = ({country:countryName, index, refetch}) => {
     const {country, countryImage, slogan} = countryName;
     console.log(country)
     return (
-        <tr>
-            <th>
+        <tr className='font-body'>
+            <th className='text-center'>
                 <p>{index + 1}</p>
             </th>
+            <td className='text-center'>
+                <img className='h-20 w-20 object-cover rounded-lg' src={countryImage} alt="" />
+            </td>
+            <td className='text-center'>
+                <p>{country}</p>
+            </td>
+            <td className='text-center'>
+                <p>{slogan}</p>
+            </td>
         </tr>
     );
 };
