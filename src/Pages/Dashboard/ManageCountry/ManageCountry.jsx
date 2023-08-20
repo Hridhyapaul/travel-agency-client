796423523de9932afc7f8c172a8791485e9db5fd
Hridhyapaul <hpdesign2021@gmail.com>
2023-08-20 16:07:31 +0600

    Implemented search filter on manage country page

--- src/Pages/Dashboard/ManageCountry/ManageCountry.jsx
+++ src/Pages/Dashboard/ManageCountry/ManageCountry.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCountry from '../../../Hooks/useCountry';
 import ManageCountryCard from './ManageCountryCard';
 import Loading from '../../../Shared/Loading';
+import { HiOutlineSearch } from 'react-icons/hi';
 
 const ManageCountry = () => {
     const [countries, countryLoading, refetch] = useCountry();
     console.log(countries)
+
+    const [query, setQuery] = useState("")
 
     if (countryLoading) {
         return <Loading></Loading>
@@ -14,6 +17,21 @@ const ManageCountry = () => {
     return (
         <div className='py-20'>
             <h2 className='text-4xl font-body font-semibold text-center pb-4'>Manage Country</h2>
+
+            {/*Start Search Input */}
+            <div className="relative w-[400px] mx-auto mt-5">
+                <input
+                    type="text"
+                    onChange={(event) => setQuery(event.target.value)}
+                    className="w-full pr-4 pl-12 py-2 border border-hoverColor rounded-md focus:ring focus:ring-designColor focus:border-designColor"
+                    placeholder="Search Accommodation Name"
+                />
+                <button className="absolute top-0 left-0 h-full px-3 flex items-center bg-designColor text-white rounded-l-md focus:outline-none focus:ring focus:ring-hoverColor">
+                    <HiOutlineSearch className='text-xl'></HiOutlineSearch>
+                </button>
+            </div>
+            {/* End Search Input */}
+
             <div className='mt-8'>
                 <div className="overflow-x-auto">
                     <table className="table font-body">
@@ -29,7 +47,9 @@ const ManageCountry = () => {
                         </thead>
                         <tbody className='bg-white'>
                             {
-                                countries.map((country, index) => <ManageCountryCard key={country._id} country={country} index={index} refetch={refetch}></ManageCountryCard>)
+                                countries
+                                    .filter(destination => destination.country.toLowerCase().includes(query.toLowerCase()))
+                                    .map((country, index) => <ManageCountryCard key={country._id} country={country} index={index} refetch={refetch}></ManageCountryCard>)
                             }
                         </tbody>
                     </table>
